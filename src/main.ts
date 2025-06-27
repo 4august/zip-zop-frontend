7af413c9c6a73c9730819bd6a4f5c9a280f4f0f3
Augusto Ferreira de Abreu <contato.augustoabreu@gmail.com>
2025-06-27 12:47:17 -0400

    primeiros componentes

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,16 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './routes';
+import { createVuetify } from 'vuetify';
+import { components, directives } from 'vuetify/dist/vuetify.js';
+
 
 const app = createApp(App);
+const vuetify = createVuetify({
+    components,
+    directives,
+})
 
+app.use(vuetify)
 app.use(router)
 app.mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+
+const count = ref(0)
 </script>
 <template>
     <h1>fala zeze bom dia</h1>
+    <v-btn color="primary" @click="count++">Hello, world! {{ count }}</v-btn>
 </template>
