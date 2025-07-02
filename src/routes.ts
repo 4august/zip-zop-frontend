3c75d1ef716cd54ab47365d2a3e49d87f1817b14
Augusto Ferreira de Abreu <contato.augustoabreu@gmail.com>
2025-07-02 08:12:43 -0400

    validação de rotas

--- src/routes.ts
+++ src/routes.ts
@@ -1,17 +1,32 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Default from "./views/Default.vue";
 import Home from "./views/Home.vue";
 import Login from "./views/Logar.vue"
+import HelloWorld from "./components/HelloWorld.vue";
 
 const routes = [
-    { path: '/', component: Default },
-    { path: '/home', component: Home },
-    { path: '/login', component: Login },
+    { path: '/', meta: { authAccess: true }, component: HelloWorld, name: "i" },
+    { path: '/home', meta: { authAccess: true }, component: Home, name: "Home" },
+    { path: '/login', meta: { authAccess: false }, component: Login, name: "Login" },
 ]
 
 const router = createRouter({
     history: createWebHistory(),
     routes
 });
+
+router.beforeEach((to, from, next) => {
+    //com flag de autenticaçao igual true e sem token de autenticaçao cai no login
+    if (to.meta.authAccess && !sessionStorage.getItem("token")) {
+
+        next({ name: "Login" });
+        return false
+    }
+    //com flag de autenticaçao igual false so segue
+    else if (!to.meta.authAccess)
+        next()
+    //so segue
+    else
+        next()
+})
 
 export default router;
