dc1b39f3b9b2063cf4a46b2467dcfdd8a6b6251d
Augusto Ferreira de Abreu <contato.augustoabreu@gmail.com>
2025-07-08 07:50:57 -0400

    alteração na api config

--- src/config/api.ts
+++ src/config/api.ts
@@ -2,14 +2,14 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
-    timeout: 1000,
-    headers: {
-        // "Access-Control-Allow-Origin": "*",
-        // "Access-Control-Allow-Headers": "Authorization",
-        // "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE",
-        // "Content-Type": "application/json;charset=UTF-8" ,
-       // 'Authorization': `${sessionStorage.getItem("token")}`
-    }
 })
+api.interceptors.request.use((config) => {
+    const token = sessionStorage.getItem("token");
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
+
 
 export default api
